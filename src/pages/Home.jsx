bdd3adedcb2d920b849a753f6d46263a16c59acf
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-10-17 11:57:57 +0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,44 +8,49 @@ function Home() {
             <section className="home">
 
                 <div className="card mb-3" >
-                    <div className="d-flex">
-                        <img src={ajax} />
-                        <div className="card-body">
+                    <div className="row row-cols-lg-2 row-cols-1">
+                        <div className="col-xl-4">
+                            <img src={ajax} />
+                        </div>
+                        <div className="col-xl-8">
+                            <div className="card-body">
 
-                            <h5>Belajar Ajax</h5>
-                            <h6>id : 1</h6>
-                            <p>5 Materi</p>
-                            <hr />
-                            <div className="row row-cols-lg-4 row-cols-md-3 row-cols-2 gap-2">
-                                <div className="col ">
-                                    <span>Apa itu html </span>
-                                    <span >  : Link Youtube</span>
-                                </div>
-                                <div className="col ">
-                                    <span>Apa itu html </span>
-                                    <span >  : Link Youtube</span>
-                                </div>
-                                <div className="col ">
-                                    <span>Apa itu html </span>
-                                    <span >  : Link Youtube</span>
-                                </div>
-                                <div className="col ">
-                                    <span>Apa itu html </span>
-                                    <span >  : Link Youtube</span>
-                                </div>
-                                <div className="col ">
-                                    <span>Apa itu html </span>
-                                    <span >  : Link Youtube</span>
-                                </div>
-                                <div className="col ">
-                                    <span>Apa itu html </span>
-                                    <span >  : Link Youtube</span>
-                                </div>
-                            </div>
+                                <h5>Belajar Ajax</h5>
+                                <h6>id : 1</h6>
+                                <p>5 Materi</p>
+                                <hr />
 
-                            <div className="button-action d-flex">
-                                <button className='btn btn-danger me-3' >DELETE</button>
-                                <button className='btn btn-primary' >UPDATE</button>
+                                <div className="row row-cols-lg-4 row-cols-md-3 row-cols-2 gap-2">
+                                    <div className="col ">
+                                        <span>Apa itu html </span>
+                                        <span >  : Link Youtube</span>
+                                    </div>
+                                    <div className="col ">
+                                        <span>Apa itu html </span>
+                                        <span >  : Link Youtube</span>
+                                    </div>
+                                    <div className="col ">
+                                        <span>Apa itu html </span>
+                                        <span >  : Link Youtube</span>
+                                    </div>
+                                    <div className="col ">
+                                        <span>Apa itu html </span>
+                                        <span >  : Link Youtube</span>
+                                    </div>
+                                    <div className="col ">
+                                        <span>Apa itu html </span>
+                                        <span >  : Link Youtube</span>
+                                    </div>
+                                    <div className="col ">
+                                        <span>Apa itu html </span>
+                                        <span >  : Link Youtube</span>
+                                    </div>
+                                </div>
+
+                                <div className="button-action d-flex">
+                                    <button className='btn btn-danger me-3' >DELETE</button>
+                                    <button className='btn btn-primary' >UPDATE</button>
+                                </div>
                             </div>
                         </div>
                     </div>
